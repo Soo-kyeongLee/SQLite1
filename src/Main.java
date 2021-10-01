17b93100a8a10437b112d4dc0a4d8342aa3d139e
Soo-kyeongLee <21900522@handong.edu>
2021-10-01 18:16:12 +0900

    1001

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello..why..did..he..");
+		Scanner s=new Scanner(System.in);
 		
 		Connection con = null;
 		
 		String dbFile="myfirst.db";
 		try {
 			//check SQlite JDBC
-			Class.forName("org.splite.JDBC");
+			Class.forName("org.sqlite.JDBC");
 			//connect with SQLite database file
 			con=DriverManager.getConnection("jdbc:sqlite:"+dbFile);
 			//query the data
@@ -29,19 +30,32 @@ public class Main {
 				System.out.println(id+" "+name+" "+regdate);
 			}
 			stat1.close();
-			
+			/*
 			//add a data
 			System.out.println("\n***새 데이터 추가***");
-			Statement stat2=con.createStatement();
-			String sql2="insert into g_artists (name, a_type, a_year, debut, regdate)"
-					+" values ('방탄소년단','남성','2010년대','2013년',datatime('now','localtime'));";
-			int cnt=stat2.executeUpdate(sql2);
-			if(cnt>0) {
-				System.out.println("new datas added");
-			}else {
-				System.out.println("Error:데이터 추가 오류~");
+			for(int i=0;i<5;i++) {
+				System.out.print("\n가수명:");
+				String name=s.nextLine();
+				System.out.print("\n성별:");
+				String a_type=s.next();
+				s.nextLine();
+				System.out.print("\n활동시기:");
+				String a_year=s.nextLine();
+				System.out.print("\n데뷔년도:");
+				String debut=s.next();
+				s.nextLine();
+				Statement stat2=con.createStatement();
+				String sql2="insert into g_artists (name, a_type, a_year, debut, regdate)"
+						+" values ('"+name+"','"+a_type+"','"+a_year+"','"+debut+"',datetime('now','localtime'));";
+				int cnt=stat2.executeUpdate(sql2);
+				if(cnt>0) {
+					System.out.println("new data added");
+				}else {
+					System.out.println("Error:데이터 추가 오류~");
+				}
+				stat2.close();
 			}
-			stat2.close();
+			
 			//edit the data
 			System.out.println("\n***데이터 수정***");
 			Statement stat3=con.createStatement();
@@ -53,8 +67,33 @@ public class Main {
 			}else {
 				System.out.println("Error:데이터 수정 오류~");
 			}
-			stat2.close();
+			stat3.close();
+					
 			//delete data
+			System.out.println("\n***데이터 삭제***");
+			Statement stat4=con.createStatement();
+			String sql4="delete from g_artists where id=4";
+			int cnt4=stat4.executeUpdate(sql4);
+			if(cnt4>0) {
+				System.out.println("success");
+			}else {
+				System.out.println("Error:데이터 삭제 오류~");
+			}
+			stat4.close();
+			
+			//query the data
+			System.out.println("\n***데이터 조회***");
+			stat1=con.createStatement();
+			spl1="select * from g_artists";
+			rs1=stat1.executeQuery(spl1);
+			while(rs1.next()) {
+				String id=rs1.getString("id");
+				String name=rs1.getString("name");
+				String a_year=rs1.getString("a_year");
+				System.out.println(id+" "+name+" "+a_year);
+			}
+			stat1.close();
+			*/
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
